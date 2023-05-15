072154864bf3ec88be30712995bf0135c7ee3de1
sampreeth <saisampreeth99@gmail.com>
2023-05-15 17:03:20 +0530

    Home Page Design & Loading Compplted

---
+++ 1-SmartContactManager-SpringBoot-Project-1/src/main/java/boot/app/controller/SmartContactManagerMiscelleniousController.java
@@ -0,0 +1,21 @@
+package boot.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/contactManager")
+public class SmartContactManagerMiscelleniousController{
+	
+	/*
+	@GetMapping("/home")
+	public String showHomePage() {
+		return "home";
+	}
+	*/
+	
+	
+}
